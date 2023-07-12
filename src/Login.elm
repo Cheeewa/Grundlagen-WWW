30683953356495b56d7ffb0f6d68333bd171f3f6
mintravadee <97973441+mintravadee@users.noreply.github.com>
2023-07-12 06:14:44 +0200

    Update Login.elm

--- src/Login.elm
+++ src/Login.elm
@@ -1,2 +1,56 @@
 module Login exposing (..)
+import Html exposing (Html, button, div, input, text)
+import Html.Attributes exposing (placeholder, type_)
+import Html.Events exposing (onClick, onInput)
+
+type alias Model =
+    { username : String
+    , password : String
+    , isLoggedIn : Bool
+    }
+
+init : Model
+init =
+    { username = ""
+    , password = ""
+    , isLoggedIn = False
+    }
+
+type Msg
+    = UpdateUsername String
+    | UpdatePassword String
+    | Login
+    | Logout
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        UpdateUsername username ->
+            { model | username = username }
+
+        UpdatePassword password ->
+            { model | password = password }
+
+        Login ->
+            -- Implement your login logic here
+            if model.username == "admin" && model.password == "admin" then
+                { model | isLoggedIn = True }
+            else
+                model
+        Logout ->
+            { model | isLoggedIn = False }
+
+view : Model -> Html Msg
+view model =
+    if model.isLoggedIn then
+        div []
+            [ text ("Logged in as: " ++ model.username)
+            , button [ onClick Logout ] [ text "Logout" ]
+            ]
+    else
+        div []
+            [ input [ type_ "text", placeholder "Username", onInput UpdateUsername ] []
+            , input [ type_ "password", placeholder "Password", onInput UpdatePassword ] []
+            , button [ onClick Login ] [ text "Login" ]
+            ]
 
